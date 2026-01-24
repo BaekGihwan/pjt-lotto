628bb60850f3e788ed35568e480457e7f2e97ddc
neur0n <ghbaek88@gmail.com>
2026-01-25 00:13:26 +0900

    recommend: step2-3 sumRange 전략 추가

--- src/modules/recommend/strategies/index.js
+++ src/modules/recommend/strategies/index.js
@@ -1,5 +1,6 @@
 const { randomStrategy } = require('./random.strategy');
 const { evenOddStrategy } = require('./evenOdd.strategy');
+const { sumRangeStrategy } = require('./sumRange.strategy');
 
 // 전략 관련 맵
 const STRATEGY_MAP = {
@@ -14,6 +15,12 @@ const STRATEGY_MAP = {
         desc: '홀짝 3:3 밸런스 전략',
         execute: (fixedNumbers, excludeNumbers) => evenOddStrategy(fixedNumbers, excludeNumbers),
     },
+
+    sumRange: {
+        key: 'sumRange',
+        desc: '합계 범위를 지정하는 전략',
+        execute: (fixedNumbers, excludeNumbers) => sumRangeStrategy(fixedNumbers, excludeNumbers),
+    }
 };
 
 function getStrategyNames() {
---
+++ src/modules/recommend/strategies/sumRange.strategy.js
@@ -0,0 +1,43 @@
+/**
+ * SumRange Recommend Strategy
+ *
+ * 로또 번호 6개의 합이 특정 범위 안에 들어오도록
+ * 랜덤 추천 결과를 필터링하는 전략
+ *
+ * 규칙
+ * - fixedNumbers는 반드시 포함
+ * - excludeNumbers는 반드시 제거
+ * - 1~45 중 중복 없이 6개 생성
+ * - 결과는 오름차순 정렬
+ * - 6개 합계가 MIN_SUM ~ MAX_SUM 범위 내
+ *
+ * 외부 상태(DB, req/res)에 의존하지 않는 순수 함수
+ * 추천 알고리즘 로직만 담당
+ *
+ */
+
+const {
+    randomStrategy
+} = require('./random.strategy');
+
+const MIN_SUM = 100;
+const MAX_SUM = 200;
+const MAX_TRY_COUNT = 500;
+
+function sumRangeStrategy(fixedNumbers = [], excludeNumbers = []) {
+
+    for (let i = 0; i < MAX_TRY_COUNT; i++) {
+        const sumRangePicked = randomStrategy(fixedNumbers, excludeNumbers);
+        const sum = sumRangePicked.reduce((acc, n) => acc + n, 0);
+
+        if(sum >= MIN_SUM && sum <= MAX_SUM) {
+            return sumRangePicked;
+        }
+    }
+
+    throw new Error(`시도횟수 ${MAX_TRY_COUNT}번을 초과 하여 번호 생성에 실패했습니다.`);
+}
+
+module.exports = {
+    sumRangeStrategy
+};
